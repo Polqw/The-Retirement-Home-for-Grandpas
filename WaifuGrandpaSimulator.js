df03ff176574d313b1cea8d1a4338198ad79ca96
Polqw <38992562+Polqw@users.noreply.github.com>
2020-05-13 16:58:37 -1000

    added interval, deleted lists

--- WaifuGrandpaSimulator.js
+++ WaifuGrandpaSimulator.js
@@ -1,95 +1,37 @@
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 var count=100;
+var myVar = setInterval(myTimer, 1000);
 
-
-var food = {
-  dessert: [
-    "cheesecake",
-    "churro",
-    "ice cream"
-    ];
-  meal: [
-    "hamburger",
-    "hot dog",
-    "spaghetti"
-    ];
-  snack: [
-    "doritos",
-    "cheetos",
-    "cookies"
-    ];
-}
-
-var magazines = {
-  finance: [
-    "stock market",
-    "bank account",
-    "how to save at 69"
-    ];
-  entertainment: [
-    "sports",
-    "movies",
-    "celebrities"
-    ];
-}:
-
-var cane = {
-  wooden: [
-    "oak",
-    "spruce",
-    "koa"
-    ];
-  plastic: [
-    "foldable",
-    "umbrella",
-    ];
-};
-
-
-var inventory = [];
-
-function store() {
-	location.href(store.html)
-}
 function consumeFood() {
-  var randType = Math.floor(Math.random(food.length));
-  var randChoice = Math.floor(Math.random(food[randType].length));
-  prompt("I would like some " + food[randType[randChoice]])
-  if () {
-  
+  var randChoice = Math.floor(Math.random()*50);
   }
-}
 
 function consumeMagazines() {
-  var randType = Math.floor(Math.random(magazines.length));
-  var randChoice = Math.floor(Math.random(magazines[randType].length));
-	prompt("I would like some " + magazines[randType[randChoice]])
-  if () {
-  
-  }
+  var randChoice = Math.floor(Math.random() * );
 }
 
 function consumeCane() {
-  var randType = Math.floor(Math.random(cane.length));
-  var randChoice = Math.floor(Math.random(cane[randType].length));
-	prompt("I would like some " + cane[randType[randChoice]])
-  if () {
-  
-  }
+  var randChoice = Math.floor(Math.random() * );
 }
 
-function happyCounter()
-{
-  count=count-1;
-  if (count <= 0)
-  {
-     clearInterval(counter);
-     //counter ended, do something here
-     return;
+function myTimer() {
+  if (count < 1) {
+   clearInterval(MyVar)
   }
-
-  //Do code for showing the number of seconds here
+  count--;
+  document.getElementById("demo").innerHTML = count;
 }
+
+function GrandpaHappinessScore() {
+    document.getElementById("GrandpaHappinessScore").innerHTML = GrandpaHappiness;
+}
+function PlayerHappinessScore() {
+    document.getElementById("PlayerHappinessScore").innerHTML = PlayerHappiness;
+}
+window.onload = GrandpaHappinessScore();
+window.onload = PlayerHappinessScore();
+
+
+
 
 
 }
